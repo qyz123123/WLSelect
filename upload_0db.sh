7008cf8890f9d9c957d82e311c8acc050a0cd1e6
qyz123123 <m18396870715@163.com>
2026-04-02 23:01:01 -0600

    recent upload

--- upload_0db.sh
+++ upload_0db.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "recent upload"
 git branch -M main
-git remote add origin "https://github.com/qyz123123/WLSelect.repo"
+git remote add origin "https://github.com/qyz123123/WLSelect.git"
 git push -u origin main
